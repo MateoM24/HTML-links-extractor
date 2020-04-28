97a8b26e2f245457be64f0ab9a02f745431d1522
mame <mame@netcompany.com>
2020-04-28 21:59:22 +0200

    Parser works. Refactor code, split into testable units.

--- main.go
+++ main.go
@@ -9,34 +9,52 @@ import (
 )
 
 func main() {
-	const fileName = "ex1.html"
+	const fileName = "ex4.html"
 	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatalln("Cannot open file:", fileName)
 	}
-	// bytes, err := ioutil.ReadAll(file)
-	// if err != nil {
-	// 	log.Fatalln("Cannot read file:", file)
-	// }
-	// fmt.Println(string(bytes))
 
 	doc, err := html.Parse(file)
 	if err != nil {
 		log.Fatalln("Cannot parse HTML file:", fileName)
 	}
 
-	var f func(*html.Node)
-	f = func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, att := range n.Attr {
-				fmt.Println(att.Key, att.Val)
+	results := make([]Result, 0, 10)
+
+	findLinkInNodes(doc, &results)
+
+	fmt.Println(results)
+
+}
+
+func findLinkInNodes(n *html.Node, results *[]Result) {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, att := range n.Attr {
+			if att.Key == "href" {
+				finding := Result{}
+				finding.link = att.Val
+				finding.text = findText(n)
+				*results = append(*results, finding)
 			}
-			fmt.Println()
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+
 		}
 	}
-	f(doc)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		findLinkInNodes(c, results)
+	}
+}
 
+func findText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		return findText(c)
+	}
+	return ""
+}
+
+type Result struct {
+	link, text string
 }
